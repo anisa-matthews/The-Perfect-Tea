974bbc4d03fd1956c3174e490089aa796520ea1d
anisa-matthews <anisa.matthews11@gmail.com>
2018-11-29 17:13:30 -0500

    fixed mongoose connect

--- db.js
+++ db.js
@@ -24,26 +24,24 @@ mongoose.model('Tea', Tea);
 mongoose.model('Tag', Tag);
 mongoose.model('User', User);
 
-mongoose.connect('mongodb://localhost/final', { useNewUrlParser: true });
+//connect to mongodb, export model
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configuration from a file
+    // use blocking file io to do this...
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, 'config.json');
+    const data = fs.readFileSync(fn);
 
-// //connect to mongodb, export model
-// // is the environment variable, NODE_ENV, set to PRODUCTION?
-// let dbconf;
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-//     // if we're in PRODUCTION mode, then read the configuration from a file
-//     // use blocking file io to do this...
-//     const fs = require('fs');
-//     const path = require('path');
-//     const fn = path.join(__dirname, 'config.json');
-//     const data = fs.readFileSync(fn);
+    // our configuration file will be in json, so parse it and set the
+    // connection string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/final';
+}
 
-//     // our configuration file will be in json, so parse it and set the
-//     // connection string appropriately!
-//     const conf = JSON.parse(data);
-//     dbconf = conf.dbconf;
-// } else {
-//     // if we're not in PRODUCTION mode, then use
-//     dbconf = 'mongodb://localhost/finalproject';
-// }
-
-// mongoose.connect(dbconf);
+mongoose.connect(dbconf,  {useNewUrlParser: true });
